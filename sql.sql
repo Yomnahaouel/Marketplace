9fbecae6b0d34168c4e00029002eb7df87524968
Seif Slimen <seifslimen@gmail.com>
2025-05-16 20:48:25 +0200

    add db & organize files

---
+++ sql.sql
@@ -0,0 +1,100 @@
+-- --------------------------------------------------------
+-- Hôte:                         127.0.0.1
+-- Version du serveur:           8.0.30 - MySQL Community Server - GPL
+-- SE du serveur:                Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Listage de la structure de la base pour marketdb
+CREATE DATABASE IF NOT EXISTS `marketdb` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `marketdb`;
+
+-- Listage de la structure de table marketdb. commande
+CREATE TABLE IF NOT EXISTS `commande` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_client` int NOT NULL,
+  `date_commande` datetime DEFAULT CURRENT_TIMESTAMP,
+  `statut` enum('En attente','Confirmée','Expédiée','Livrée','Annulée') DEFAULT 'En attente',
+  `montant_total` decimal(10,2) NOT NULL,
+  `adresse_livraison` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_client` (`id_client`),
+  CONSTRAINT `commande_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table marketdb.commande : ~0 rows (environ)
+
+-- Listage de la structure de table marketdb. panier
+CREATE TABLE IF NOT EXISTS `panier` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_client` int NOT NULL,
+  `date_creation` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id_client` (`id_client`),
+  CONSTRAINT `panier_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table marketdb.panier : ~0 rows (environ)
+
+-- Listage de la structure de table marketdb. produit
+CREATE TABLE IF NOT EXISTS `produit` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `description` text,
+  `prix` decimal(10,2) NOT NULL,
+  `quantite_stock` int NOT NULL,
+  `id_vendeur` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_vendeur` (`id_vendeur`),
+  CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`id_vendeur`) REFERENCES `utilisateur` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table marketdb.produit : ~0 rows (environ)
+
+-- Listage de la structure de table marketdb. publicite
+CREATE TABLE IF NOT EXISTS `publicite` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_produit` int NOT NULL,
+  `date_debut` date NOT NULL,
+  `date_fin` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_produit` (`id_produit`),
+  CONSTRAINT `publicite_ibfk_1` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table marketdb.publicite : ~0 rows (environ)
+
+-- Listage de la structure de table marketdb. utilisateur
+CREATE TABLE IF NOT EXISTS `utilisateur` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `type` enum('Admin','Vendeur','Client') NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Listage des données de la table marketdb.utilisateur : ~5 rows (environ)
+INSERT INTO `utilisateur` (`id`, `nom`, `email`, `mot_de_passe`, `type`) VALUES
+	(1, 'Admin', 'admin@admin.com', '1234', 'Admin'),
+	(6, 'admin1', 'admin@marketplace.com', 'mdp123', 'Admin'),
+	(7, 'vendeur1', 'vendeur@marketplace.com', 'mdp123', 'Vendeur'),
+	(8, 'client1', 'client@marketplace.com', 'mdp123', 'Client'),
+	(12, 'hefny', 'hefny@gmail.com', '1234', 'Vendeur');
+
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
